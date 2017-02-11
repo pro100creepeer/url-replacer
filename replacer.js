53cb1b80483df5416b6d41c9e5f12d6659cdafc0
Doctor <kostomarov1999@gmail.com>
2017-02-12 00:21:52 +0300

    Update replacer.js

--- replacer.js
+++ replacer.js
@@ -12,17 +12,16 @@ inputFilename  = process.argv[inputFilename + 1];
 outputFilename = process.argv[outputFilename + 1];
 fs.writeFileSync(
   outputFilename,
-  fs.readFileSync(inputFilename).toString().replace(/url\(\'?(.+?)\'?\);/gi, (word, $1) => {
+  fs.readFileSync(inputFilename).toString().replace(/url\(\'?(.+?)\'?\)/gi, (word, $1) => {
     if(!$1.startsWith('http')) return 'url(' + $1 + ');';
     let fn = $1.split('/');
     fn     = fn[fn.length - 1];
     download($1, fn);
-    if(verbose) console.log('Replaced' + $1 + ' -> ' + '/views/images/styles/' + fn);
+    if(verbose) console.log('Replaced ' + $1 + ' -> ' + '/views/images/styles/' + fn);
     return 'url(/views/images/styles/' + fn + ');';
   }));
 
 function download(finame, foname) {
-  child_process.exec('curl ' + finame, (e, sin) => {
-    fs.writeFileSync(foname, sin);
-  })
+  child_process.exec('rm -rf images/*')
+  child_process.exec('wget -P images ' + finame)
 }
